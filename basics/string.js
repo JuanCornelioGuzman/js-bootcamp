29dd25e558a2b5916dfa37e9e424555f32f471fc
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2019-09-18 11:29:35 +0800

    String and Variables

---
+++ basics/string.js
@@ -0,0 +1,12 @@
+let firstName = 'JC';
+let lastName = 'Guzman';
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+let city = 'Pasay';
+let country = 'Philippines';
+let location = city + ', ' + country;
+
+console.log(location);
+
