8b35e092b0537771f6d8a77321478957217b7716
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2019-09-19 11:28:15 +0800

    Numbers

---
+++ basics/numbers.js
@@ -0,0 +1,18 @@
+let age = 28
+let dogYear = age / 7
+
+console.log(dogYear)
+
+// Challenge are
+
+// stundentScore 18
+// maxScore 20
+// percent 90
+
+// print the percent
+
+let stundentScore = 18
+let maxScore = 20
+let percent = ( stundentScore / maxScore ) * 100
+
+console.log(percent+'%')
