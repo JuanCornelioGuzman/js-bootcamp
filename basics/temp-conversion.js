9acce624cbabe4202817c6c70efca82ae8925420
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2019-09-20 10:43:27 +0800

    Temperature Converter

---
+++ basics/temp-conversion.js
@@ -0,0 +1,11 @@
+let fahrenheit = 32 // Oc - 273.15K
+
+// calculate celsius and store in celsius variable
+let celsius = (fahrenheit - 32) * 5/9
+// print that value
+console.log(celsius)
+
+// calculate celsius and store in variable
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+// print that value
+console.log(kelvin)
